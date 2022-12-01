a21552c600a08cf5002244e510142018768af192
Adnan0707 <115894639+Adnan0707@users.noreply.github.com>
2022-12-01 19:44:49 +0600

    Create Table.sql

---
+++ mysql/Table.sql
@@ -0,0 +1,18 @@
+create table BB_Manager
+( M_id int NOT NULL,
+ mName varchar(max) NOT NULL,
+ m_phNo bigint
+);
+
+INSERT into BB_Manager
+VALUES(001,'AA', 0123456789),
+(002,'BB', 1234567890),
+(003,'CC', 2345678901),
+(004,'DD', 3456789012);
+
+INSERT into BB_Manager
+VALUES(005,'EE', 4694959671),
+(006,'FF', 4695959671),
+(007,'GG', 4663959671),
+(008,'HH', 4673959671),
+(009,'II', 4693859671);
