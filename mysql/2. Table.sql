a0ebff774d618794251e989325f4b811941dda34
Adnan0707 <115894639+Adnan0707@users.noreply.github.com>
2022-12-01 19:47:19 +0600

    Update 2. Table.sql

--- mysql/2. Table.sql
+++ mysql/2. Table.sql
@@ -1,15 +1,13 @@
 create table BB_Manager
 ( M_id int NOT NULL,
- mName varchar(max) NOT NULL,
+ mName varchar(20) NOT NULL,
  m_phNo bigint
 );
-
 INSERT into BB_Manager
 VALUES(001,'AA', 0123456789),
 (002,'BB', 1234567890),
 (003,'CC', 2345678901),
 (004,'DD', 3456789012);
-
 INSERT into BB_Manager
 VALUES(005,'EE', 4694959671),
 (006,'FF', 4695959671),
