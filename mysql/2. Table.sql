afc6d52424b9d19bcee42fae893d043a3d58c695
Adnan0707 <115894639+Adnan0707@users.noreply.github.com>
2022-12-10 11:48:34 +0600

    Update 2. Table.sql

--- mysql/2. Table.sql
+++ mysql/2. Table.sql
@@ -8,14 +8,6 @@ CREATE TABLE BB_Manager
 INSERT into BB_Manager
 VALUES(102,'M1', 0175),
 (103,'M2', 0174),
-(104,'M3', 0176),
-(105,'M4', 0167);
-INSERT into BB_Manager
-VALUES(106,'M5', 0187),
-(107,'M6', 0198),
-(108,'M7', 0188),
-(109,'M8', 0199),
-(110,'M9', 0166);
 select * from BB_Manager
 
 /* Table 2 by Adnan */
@@ -32,12 +24,23 @@ CREATE TABLE Blood_Donor
 INSERT into Blood_Donor
 VALUES(201,'BD1',25,'M','B+','2021-12-17',301,1100),
 (202,'BD2',35,'F','A+','2021-11-22',302,1100),
-(203,'BD3',22,'M','O+','2021-01-04',303,1200),
-(204,'BD4',29,'M','O+','2021-07-19',304,1300),
-(205,'BD5',42,'F','A-','2021-12-24',305,1300),
-(206,'BD6',44,'M','AB+','2021-08-28',306,1200),
-(207,'BD7',33,'F','AB-','2022-02-06',307,1400),
-(208,'BD8',31,'F','AB+','2022-09-10',308,1200),
-(209,'BD9',24,'M','B-','2022-10-15',309,1500),
-(210,'BD10',29,'M','O-','2022-12-17',310,1200);
 select * from Blood_Donor
+
+/* Table 3 by Adnan */
+/*r BloodSpecimen entity we will store the information of blood samples which 
+are available in the blood bank
+*/
+CREATE TABLE BloodSpecimen
+( specimen_number int NOT NULL,
+ b_group varchar(10) NOT NULL,
+ status int,
+ dfind_ID int NOT NULL,
+ M_id int NOT NULL,
+ PRIMARY KEY (specimen_number,b_group)
+);
+
+INSERT into BloodSpecimen
+VALUES(1001, 'B+', 1,11,101),
+(1002, 'O-', 1,12,102);
+Select * from BloodSpecimen;
+
