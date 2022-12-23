05ffab37e64395d9d8d016aae9adc94502339dec
ruhan2022 <117507553+ruhan2022@users.noreply.github.com>
2022-12-23 23:22:22 +0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,4 @@
 
+INSERT INTO `donor_information` (`name`, `phoneNumber`, `bloodGroup`, `age`, `email`, `password`) VALUES ('new user', '12345678', 'a+', '25', 'newUSer@gmail.com', '12345');
+
+INSERT INTO `donor_information` (`name`, `phoneNumber`, `bloodGroup`, `age`, `email`, `password`) VALUES ('rakib', '0170000009', 'a+', '25', 'rakib@gmail.com', '12345');
