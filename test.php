2634b4347cda5e183b31d30d9e4ca45e316e9512
ruhan2022 <117507553+ruhan2022@users.noreply.github.com>
2022-12-23 23:19:46 +0600

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,19 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action=delete.php" method="POST">
+
+<input type="text" name="someText" >
+
+<button type="submit" value="Submit" name="submit">sdffds</button>
+
+</form>
+</body>
+</html>
