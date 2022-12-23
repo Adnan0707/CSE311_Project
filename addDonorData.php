3c3c4e773365cccef6eb36ca46facf32f92f5ba7
ruhan2022 <117507553+ruhan2022@users.noreply.github.com>
2022-12-23 23:12:15 +0600

    Update addDonorData.php

--- addDonorData.php
+++ addDonorData.php
@@ -6,11 +6,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body style="background-color:black">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
 </body>
 
 
@@ -19,7 +22,9 @@
     <nav class="navbar navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -27,9 +32,7 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="/">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/reg.php">Create New Account</a>
-                    </li>
+                    
 
 
 
@@ -53,7 +56,7 @@
         <br>
         <div class="form-group">
             <label>Donor Name</label>
-            <input class="form-control" name="donorName" id="donorName" placeholder="Enter your name">
+            <input class="form-control" name="name" placeholder="Enter your name">
 
         </div>
         <br>
@@ -65,7 +68,8 @@
         <br>
         <div class="form-group">
             <label>Age</label>
-            <input type="number" name="age" class="form-control" id="age" placeholder="Age ">
+            <input type="number" name="age" class="form-control" id="age"
+                placeholder="Age ">
 
         </div>
         <br>
@@ -76,7 +80,7 @@
         </div>
         <div class="form-group">
             <label>Blood Group</label>
-            <input name="bloodGroup" class="form-control" id="bloodGroup" placeholder="Enter Blood group">
+            <input  name="bloodGroup" class="form-control" id="bloodGroup" placeholder="Enter Blood group">
 
         </div>
         <br>
@@ -89,7 +93,16 @@
 
         <button type="submit" name="sava_donor" class="btn btn-primary">Sign up</button>
 
+
+
+
         <br>
+
+
+
+
+               
+<!--        <button type="submit" class="btn btn-primary" style="margin-left: 450px;">Sign in</button>-->
 
     </form>
 
