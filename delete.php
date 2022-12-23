c6feff0d9a4fab3cc826e1f14bc853ddefa34ec3
ruhan2022 <117507553+ruhan2022@users.noreply.github.com>
2022-12-23 23:19:11 +0600

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,25 @@
 
+<?php
+require 'dbConnect.php';
+
+
+if(isset($_POST['delete_donor']))
+{
+    $email = mysqli_real_escape_string($con,$_POST["email"]);   
+     
+  //  echo "$test";
+
+//DELETE FROM `donor_information` WHERE email="newUSer@gmail.com";
+//echo "$email";
+
+$sql = 'DELETE FROM `donor_information` WHERE email="$email"';
+
+
+if ($con->query($sql) === TRUE) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error deleting record: " . $conn->error;
+}
+
+}
+?>
